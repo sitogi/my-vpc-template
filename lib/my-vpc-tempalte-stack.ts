9451394d00e9b7c47a315f7e48c63289f9d9e365
sitogi <takano162@gmail.com>
2021-10-26 05:14:57 +0900

    First commit

--- lib/my-vpc-tempalte-stack.ts
+++ lib/my-vpc-tempalte-stack.ts
@@ -1,9 +1,81 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+
+const MY_GLOBAL_IP = 'xxx.xxx.xxx.xxx/0'
+
+const PREFIX = 'my-app-';
+const VPC_ID = `${PREFIX}vpc`;
+const PUBLIC_SUBNET_FOR_ALB_ID = `${PREFIX}public-subnet-for-alb`;
+const PRIVATE_SUBNET_FOR_APP_ID = `${PREFIX}public-subnet-for-app`;
+const PRIVATE_SUBNET_FOR_DB_ID = `${PREFIX}public-subnet-for-db`;
+const PRIVATE_SUBNET_FOR_BASTION_ID = `${PREFIX}public-subnet-for-bastion`;
+const SG_FOR_ALB_ID = `${PREFIX}sg-for-alb`;
+const SG_FOR_APP_ID = `${PREFIX}sg-for-app`;
+const SG_FOR_DB_ID = `${PREFIX}sg-for-db`;
+const SG_FOR_BASTION_ID = `${PREFIX}sg-for-bastion`;
 
 export class MyVpcTempalteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.createVpc();
   }
+
+  // VPC, Subnet, SecurityGroup の作成
+  private createVpc() {
+    const vpc = new ec2.Vpc(this, VPC_ID, {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: PUBLIC_SUBNET_FOR_ALB_ID,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: PRIVATE_SUBNET_FOR_APP_ID,
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+        {
+          cidrMask: 24,
+          name: PRIVATE_SUBNET_FOR_DB_ID,
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+        {
+          cidrMask: 24,
+          name: PRIVATE_SUBNET_FOR_BASTION_ID,
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
+    });
+
+    const sgForAlb = new ec2.SecurityGroup(this, SG_FOR_ALB_ID, {
+      vpc: vpc,
+      securityGroupName:  PUBLIC_SUBNET_FOR_ALB_ID
+    });
+    sgForAlb.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80));
+
+    const sgForApp = new ec2.SecurityGroup(this, SG_FOR_APP_ID, {
+      vpc: vpc,
+      securityGroupName:  PRIVATE_SUBNET_FOR_APP_ID
+    });
+    sgForApp.addIngressRule(sgForAlb, ec2.Port.tcp(80));
+
+    const sgForDb = new ec2.SecurityGroup(this, SG_FOR_DB_ID, {
+      vpc: vpc,
+      securityGroupName:  PRIVATE_SUBNET_FOR_DB_ID
+    });
+    sgForDb.addIngressRule(sgForApp, ec2.Port.tcp(3306));
+
+    const sgForBastion = new ec2.SecurityGroup(this, SG_FOR_BASTION_ID, {
+      vpc: vpc,
+      securityGroupName:  PRIVATE_SUBNET_FOR_BASTION_ID
+    });
+    sgForBastion.addIngressRule(ec2.Peer.ipv4(MY_GLOBAL_IP), ec2.Port.tcp(22));
+
+    return vpc;
+  }
+
 }
+
