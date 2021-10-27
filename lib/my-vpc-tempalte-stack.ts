b4ce1cbb3d9abac1bc2f0e74aa9cdce880c588bc
sitogi <takano162@gmail.com>
2021-10-27 18:02:10 +0900

    Add ALB, Target Group, Listener

--- lib/my-vpc-tempalte-stack.ts
+++ lib/my-vpc-tempalte-stack.ts
@@ -1,13 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
 
 const PREFIX = 'my-app-';
 const VPC_ID = `${PREFIX}vpc`;
 // Subnet 名には自動で VPC_ID が付与されるのでここでは PREFIX は付けない
 const PUBLIC_SUBNET_FOR_ALB_ID = `public-for-alb`;
+const PUBLIC_SUBNET_FOR_ALB2_ID = `public-for-alb2`;
 const PRIVATE_SUBNET_FOR_APP_ID = `private-for-app`;
+const PRIVATE_SUBNET_FOR_APP2_ID = `private-for-app2`;
 const PRIVATE_SUBNET_FOR_DB_ID = `private-for-db`;
+const PRIVATE_SUBNET_FOR_DB2_ID = `private-for-db2`;
 const PRIVATE_SUBNET_FOR_BASTION_ID = `private-subnet-for-bastion`;
+const PRIVATE_SUBNET_FOR_BASTION2_ID = `private-subnet-for-bastion2`;
 const SG_FOR_ALB_ID = `${PREFIX}sg-for-alb`;
 const SG_FOR_APP_ID = `${PREFIX}sg-for-app`;
 const SG_FOR_DB_ID = `${PREFIX}sg-for-db`;
@@ -18,6 +23,9 @@ const VPC_ENDPOINT_LOGS = `${PREFIX}vpc-endpoint-logs`;
 const VPC_ENDPOINT_ECR_DKR = `${PREFIX}vpc-endpoint-ecr-dkr`;
 const VPC_ENDPOINT_ECR_API = `${PREFIX}vpc-endpoint-ecr-api`;
 const VPC_ENDPOINT_SSM = `${PREFIX}vpc-endpoint-ssm`;
+const ALB_ID = `${PREFIX}alb`;
+const TARGET_GROUP_ID = `${PREFIX}-target-group`;
+const ALB_LISTENER_ID = `${PREFIX}-alb-listener`;
 
 export class MyVpcTempalteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -26,37 +34,80 @@ export class MyVpcTempalteStack extends cdk.Stack {
     this.createVpc();
   }
 
-  // VPC, Subnet, SecurityGroup の作成
   private createVpc() {
-    // TODO: ルートテーブルが出来すぎているのでなんとかしたい
+    // VPC
     const vpc = new ec2.Vpc(this, VPC_ID, {
       cidr: "10.0.0.0/16",
       natGateways: 0,
-      // 自動的に 2 つの AZ 上に構築される
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: PUBLIC_SUBNET_FOR_ALB_ID,
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: PRIVATE_SUBNET_FOR_APP_ID,
-          subnetType: ec2.SubnetType.ISOLATED,
-        },
-        {
-          cidrMask: 24,
-          name: PRIVATE_SUBNET_FOR_DB_ID,
-          subnetType: ec2.SubnetType.ISOLATED,
-        },
-        {
-          cidrMask: 24,
-          name: PRIVATE_SUBNET_FOR_BASTION_ID,
-          subnetType: ec2.SubnetType.ISOLATED,
-        },
-      ],
+      subnetConfiguration: [], // 自動で作成されないように明示的に空にしておく
     });
 
+    // Subnets
+    const albSubnet1 = new ec2.Subnet(this, PUBLIC_SUBNET_FOR_ALB_ID, {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.0.0/24"
+    });
+
+    const albSubnet2 = new ec2.Subnet(this, PUBLIC_SUBNET_FOR_ALB2_ID, {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.1.0/24"
+    });
+
+    const igw = new ec2.CfnInternetGateway(this, "InternetGateway", {});
+    new ec2.CfnVPCGatewayAttachment(this, "gateway", {
+      vpcId: vpc.vpcId,
+      internetGatewayId: igw.ref
+    });
+
+    albSubnet1.addRoute("ALBSubnetRoute1", {
+      routerType: ec2.RouterType.GATEWAY,
+      routerId: igw.ref
+    });
+
+    albSubnet2.addRoute("ALBSubnetRoute2", {
+      routerType: ec2.RouterType.GATEWAY,
+      routerId: igw.ref
+    });
+
+    const appSubnet1 = new ec2.Subnet(this, PRIVATE_SUBNET_FOR_APP_ID, {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.2.0/24"
+    });
+
+    const appSubnet2 = new ec2.Subnet(this, PRIVATE_SUBNET_FOR_APP2_ID, {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.3.0/24"
+    });
+
+    new ec2.Subnet(this, PRIVATE_SUBNET_FOR_DB_ID, {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.4.0/24"
+    });
+
+    new ec2.Subnet(this, PRIVATE_SUBNET_FOR_DB2_ID, {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.5.0/24"
+    });
+
+    new ec2.Subnet(this, PRIVATE_SUBNET_FOR_BASTION_ID, {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.6.0/24"
+    });
+
+    new ec2.Subnet(this, PRIVATE_SUBNET_FOR_BASTION2_ID, {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.7.0/24"
+    });
+
+    // Security Groups
     const sgForAlb = new ec2.SecurityGroup(this, SG_FOR_ALB_ID, {
       vpc: vpc,
       securityGroupName:  SG_FOR_ALB_ID
@@ -86,34 +137,57 @@ export class MyVpcTempalteStack extends cdk.Stack {
       securityGroupName:  SG_FOR_EGRESS_ID
     });
 
-    // TODO: Subnet の指定 (なんでいい感じに app に紐付けられてたかは不明)
     vpc.addInterfaceEndpoint(VPC_ENDPOINT_ECR_DKR, {
       service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
-      // subnets: [],
+      subnets: { subnets: [appSubnet1, appSubnet2] },
       securityGroups: [sgForVpcEndpoints]
     });
 
     vpc.addInterfaceEndpoint(VPC_ENDPOINT_ECR_API, {
       service: ec2.InterfaceVpcEndpointAwsService.ECR,
-      // subnets: [],
+      subnets: { subnets: [appSubnet1, appSubnet2] },
       securityGroups: [sgForVpcEndpoints]
     });
 
     vpc.addInterfaceEndpoint(VPC_ENDPOINT_LOGS, {
       service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
-      // subnets: [],
+      subnets: { subnets: [appSubnet1, appSubnet2] },
       securityGroups: [sgForVpcEndpoints]
     });
 
     vpc.addInterfaceEndpoint(VPC_ENDPOINT_SSM, {
       service: ec2.InterfaceVpcEndpointAwsService.SSM,
-      // subnets: [ec2.Subnet.fromSubnetAttributes(this, 'aaa', {  })],
+      subnets: { subnets: [appSubnet1, appSubnet2] },
       securityGroups: [sgForVpcEndpoints]
     });
 
     vpc.addGatewayEndpoint(VPC_ENDPOINT_S3, {
       service: ec2.GatewayVpcEndpointAwsService.S3,
-      // subnets: [],
+      subnets: [{ subnets: [appSubnet1, appSubnet2] }],
+    });
+
+    // Application Load Balancer
+    const alb = new elb.ApplicationLoadBalancer(this, ALB_ID, {
+        vpc: vpc,
+        internetFacing: true,
+        ipAddressType: elb.IpAddressType.IPV4,
+        loadBalancerName: ALB_ID,
+        securityGroup: sgForAlb,
+        vpcSubnets: { subnets: [albSubnet1, albSubnet2] }
+    });
+
+    const targetGroup = new elb.ApplicationTargetGroup(this, TARGET_GROUP_ID, {
+      targetGroupName: TARGET_GROUP_ID,
+      vpc: vpc,
+      targetType: elb.TargetType.IP,
+      protocol: elb.ApplicationProtocol.HTTP,
+      port: 80,
+    });
+
+    new elb.ApplicationListener(this, ALB_LISTENER_ID, {
+      loadBalancer: alb,
+      port: 80,
+      defaultTargetGroups: [targetGroup]
     });
   }
 }
