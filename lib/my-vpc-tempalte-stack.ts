ce3b082926a8300b5b5f2a85eac9b845c55a4c18
sitogi <takano162@gmail.com>
2021-10-27 15:42:28 +0900

    Add VPC Endpoints

--- lib/my-vpc-tempalte-stack.ts
+++ lib/my-vpc-tempalte-stack.ts
@@ -12,6 +12,12 @@ const SG_FOR_ALB_ID = `${PREFIX}sg-for-alb`;
 const SG_FOR_APP_ID = `${PREFIX}sg-for-app`;
 const SG_FOR_DB_ID = `${PREFIX}sg-for-db`;
 const SG_FOR_BASTION_ID = `${PREFIX}sg-for-bastion`;
+const SG_FOR_EGRESS_ID = `${PREFIX}sg-for-egress`; // for VPC Endpoints
+const VPC_ENDPOINT_S3 = `${PREFIX}vpc-endpoint-s3`;
+const VPC_ENDPOINT_LOGS = `${PREFIX}vpc-endpoint-logs`;
+const VPC_ENDPOINT_ECR_DKR = `${PREFIX}vpc-endpoint-ecr-dkr`;
+const VPC_ENDPOINT_ECR_API = `${PREFIX}vpc-endpoint-ecr-api`;
+const VPC_ENDPOINT_SSM = `${PREFIX}vpc-endpoint-ssm`;
 
 export class MyVpcTempalteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -22,6 +28,7 @@ export class MyVpcTempalteStack extends cdk.Stack {
 
   // VPC, Subnet, SecurityGroup の作成
   private createVpc() {
+    // TODO: ルートテーブルが出来すぎているのでなんとかしたい
     const vpc = new ec2.Vpc(this, VPC_ID, {
       cidr: "10.0.0.0/16",
       natGateways: 0,
@@ -73,8 +80,41 @@ export class MyVpcTempalteStack extends cdk.Stack {
       securityGroupName:  SG_FOR_BASTION_ID
     });
 
-    return vpc;
-  }
+    // TODO: Ingress 443 がついてた...？
+    const sgForVpcEndpoints = new ec2.SecurityGroup(this, SG_FOR_EGRESS_ID, {
+      vpc: vpc,
+      securityGroupName:  SG_FOR_EGRESS_ID
+    });
 
+    // TODO: Subnet の指定 (なんでいい感じに app に紐付けられてたかは不明)
+    vpc.addInterfaceEndpoint(VPC_ENDPOINT_ECR_DKR, {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+      // subnets: [],
+      securityGroups: [sgForVpcEndpoints]
+    });
+
+    vpc.addInterfaceEndpoint(VPC_ENDPOINT_ECR_API, {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+      // subnets: [],
+      securityGroups: [sgForVpcEndpoints]
+    });
+
+    vpc.addInterfaceEndpoint(VPC_ENDPOINT_LOGS, {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+      // subnets: [],
+      securityGroups: [sgForVpcEndpoints]
+    });
+
+    vpc.addInterfaceEndpoint(VPC_ENDPOINT_SSM, {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+      // subnets: [ec2.Subnet.fromSubnetAttributes(this, 'aaa', {  })],
+      securityGroups: [sgForVpcEndpoints]
+    });
+
+    vpc.addGatewayEndpoint(VPC_ENDPOINT_S3, {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+      // subnets: [],
+    });
+  }
 }
 
